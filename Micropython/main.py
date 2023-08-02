d98a01cd0c3ea346e64957105df9a1fa628c61ae
Jean-Claude Feltes <jean-claude.feltes@education.lu>
2023-08-02 10:50:19 +0200

    Add files via upload

--- Micropython/main.py
+++ Micropython/main.py
@@ -1,5 +1,10 @@
 import blink
 blink.blink(3, dt = 0.1)
 
-from measureUIP2x_02 import *
-continuous_measure()
+#from test_mpp_06 import *
+
+#main()
+
+#import mpptrack_01 
+#import test_oled
+import mpptrack_02
